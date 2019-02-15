182f58250882724932505ee3e6f1c533616e58a1
putty182 <putty182@gmail.com>
2019-02-15 20:51:57 +1000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,14 +2,15 @@
 LOCAL_FORWARDING="";
 
 for PORT in "$PORTS"; do
-    echo LOCAL_FORWARDING="$LOCAL_FORWARDING -L '*:$PORT:${BIND_ADDRESS:-127.0.0.1}:PORT'"
+    LOCAL_FORWARDING="$LOCAL_FORWARDING -L '*:$PORT:${BIND_ADDRESS:-127.0.0.1}:PORT'"
 done
 
 
-ssh -o StrictHostKeyChecking=no \
+ssh -v \
+    -o StrictHostKeyChecking=no \
     -o UserKnownHostsFile=/dev/null \
     -o GlobalKnownHostsFile=/dev/null \
     -o ServerAliveInterval=${KEEP_ALIVE:-180} \
     -i /private-ssh-key \
-    ${LOCAL_FORWARDING} \
+    $LOCAL_FORWARDING \
     "$USERNAME@$REMOTE_HOST" -N
