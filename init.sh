d0d940a12f30c3ebb850cd6d636b37defc3c52b6
putty182 <putty182@gmail.com>
2019-03-13 22:43:50 +1000

    added reverse/remote forwarding

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 LOCAL_FORWARDING="";
+REMOTE_FORWARDING="";
 
 for PORT in $PORTS; do
     LOCAL_FORWARDING="$LOCAL_FORWARDING -L *:$PORT:localhost:$PORT "
+done
+
+for REVERSE_PORT in $REVERSE_PORTS; do
+    REMOTE_FORWARDING="$REMOTE_FORWARDING -R *:$REVERSE_PORT:localhost:$REVERSE_PORT "
 done
 
 
@@ -13,4 +18,5 @@ ssh -v \
     -o ServerAliveInterval=${KEEP_ALIVE:-180} \
     -i /private-ssh-key \
     $LOCAL_FORWARDING \
+    $REMOTE_FORWARDING \
     "$USERNAME@$REMOTE_HOST" -N
